848c84ea718440d0ecbf6fcdf0b859d9769af637
Alexander VALERIANO <valerianoalexander@gmail.com>
2023-01-18 14:03:07 +0100

    Config test suite exemple pour jenkins

---
+++ Test Suites/TestSuiteDemoOne.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteDemoOne</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9b314880-45ee-4eaf-8f3a-3e7830877d56</testSuiteGuid>
+   <testCaseLink>
+      <guid>1c6b5352-216d-4732-a486-b09c872f9dff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DemoTests/Test3_ScriptMode</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
